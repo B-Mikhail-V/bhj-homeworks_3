25ae1643e4b213129fbfc4a7a3004af9e0b3cc92
B-Mikhail-V <bakulin.mv@gmail.com>
2022-09-19 22:59:26 +0300

    Task1-2

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,32 +1,23 @@
 const btn1 = Array.from(document.querySelectorAll('.dropdown__value'))
-const lst1 = document.querySelector('.dropdown__list')
+const lst1 = Array.from(document.querySelectorAll('.dropdown__list'))
 const items = Array.from(document.querySelectorAll('.dropdown__item'))
 
-console.log(btn1);
 
-function onClick1() {
-    lst1.classList.toggle('dropdown__list_active');
-    // console.log('OK');
-}
-    
-
-for(let i = 0; i < btn1.length; i++) {
-    btn1[i].addEventListener('click', onClick1)
-    console.log(btn1[i].textContent);
+function onClick1(e) {
+    e.target.nextElementSibling.classList.toggle('dropdown__list_active');
 }
 
+function onClick2(e2) {
+    let el2 = e2.target
+    console.log(el2);
+}
 
+btn1.forEach(element => {
+    element.addEventListener('click', onClick1)
+});
 
-
-// function onClick2(i) {
-//     btn1.textContent = item[i].textContent;
-
-// }
-
-
-// for(let i = 0; i < items.length; i++) {
-
-//     items[i].addEventListener("click", onClick2);
-//     }
-
+items.forEach(element => {
+    console.log(element);
+    element.addEventListener('click', onClick2)
+})
 
